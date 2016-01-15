12e0a2f26b2d224709a8ccdc2784e96c21d834f8
Steve Smith <steve@kentsmiths.com>
2016-01-14 20:45:12 -0500

    Adding sample.

---
+++ mvc/performance/response-caching/sample/src/ResponseCacheSample/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace ResponseCacheSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 10, Location = ResponseCacheLocation.Any, NoStore = false)]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+
+}
